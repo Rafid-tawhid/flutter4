bcbb32a134ae5da473d995d717a3d0c8ae01c116
Tawhidur Rahman Rafid <ilovemymom12345>
2021-06-07 17:03:25 +0600

    login page design

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,92 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      color: Colors.white,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.cyanAccent,
-          title: Text("AppBar"),
-          actions: [
-            IconButton(icon: Icon(Icons.search), onPressed: (){}),
-            IconButton(icon: Icon(Icons.add), onPressed: (){}),
-            IconButton(icon: Icon(Icons.call_to_action), onPressed: (){}),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(child: Text("Settings"),
-              decoration: BoxDecoration(
-                color: Colors.red
-              ),),
-              ListTile(
-                title: Text("Active member"),
-                onTap: (){},
-              ),
-              ListTile(
-                title: Text("Lazy member"),
-                onTap: (){},
-              ),
-              ListTile(
-                title: Text("Deactive member"),
-                onTap: (){},
-              )
 
-            ],
-          ),
+        body: Container(
+          child: LoginPage(),
         ),
-      )
+      ),
     );
   }
 }
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 100
+                  ),
+                  child: Text(
+
+                      "Learn Free",
+                    style:TextStyle(
+                      fontSize: 30,color: Colors.blue
+                    ) ,
+                  ),
+                ),
+                Container(padding:EdgeInsets.all(10),margin:EdgeInsets.all(15),child: Text("Learning is the best way to express yourself and its also good for to know your limit where to speak or not",style: TextStyle(fontSize: 16),textAlign: TextAlign.center,))
+
+              ],
+            )
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 40
+            ),
+            child: Center(child: Image.asset("assets/images/bg.jpg")),
+          ),
+          Container(
+            child: Container(
+              width: double.infinity,
+
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(17),
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(30)
+              ),
+              child: Center(child: Text("Get Started",style: TextStyle(color: Colors.white,fontSize: 16),)),
+
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
 
